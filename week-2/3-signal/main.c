8e94e7db0eded13b2a977d04a418ca6cb33728e9
crazyfrog-phongee <trongphong1708@gmail.com>
2023-02-18 00:53:54 +0700

    Finish assignment 3.1: Blocking SIGINT program

--- week-2/3-signal/main.c
+++ week-2/3-signal/main.c
@@ -2,9 +2,12 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
+#include <stdint.h>
 
 void sig_handler1(int signum);
+int sigisemptyset(sigset_t *setp);
+void dec_to_binary(unsigned long int num);
+void print_set_word(sigset_t *setp);
 
 int main()
 {
@@ -15,35 +18,34 @@ int main()
 		fprintf(stderr, "Cannot handle SIGINT\n");
 		exit(EXIT_FAILURE);
 	}
-	
 
 	sigemptyset(&newset);
 	sigemptyset(&oldset);
 
-	printf("Current signal mask of the process: %x", oldset);
-
 	sigaddset(&newset, SIGINT);
 
-	if (sigprocmask(SIG_SETMASK, &newset, &oldset) == 0)
+	if (sigprocmask(SIG_SETMASK, &newset, NULL) == 0)
 	{
-		if (sigismember(&newset, SIGINT) == 1)
+		sigprocmask(SIG_SETMASK, NULL, &oldset); /* Get the current value of the signal mask returned in oldset */
+
+		printf("Current signal mask of the process in HEX: ");
+		print_set_word(&oldset); /* Print the current signal mask of the process in HEX */
+		printf("Current signal mask of the process in BINARY: ");
+		dec_to_binary(oldset.__val[0]); /* Print the current signal mask of the process in BINARY */
+
+		/* Check SIGINT is member of set */
+		if (sigismember(&oldset, SIGINT) == 1)
 		{
 			printf("SIGINT is a member of set\n");
 		}
-		else if (sigismember(&newset, SIGINT) == 0)
+		else if (sigismember(&oldset, SIGINT) == 0)
 		{
 			printf("SIGINT is not a member of set\n");
 		}
-		else if (sigismember(&newset, SIGINT) == -1)
-		{
-			printf("On error\n");
-		}
 	}
 
-	/* Print current signal mask of the process */
-	printf("Current signal mask of the process: %x", oldset);
-	
-	while(1);
+	while (1)
+		;
 
 	return 0;
 }
@@ -52,4 +54,47 @@ void sig_handler1(int signum)
 {
 	printf("\nIm signal handler1: %d\n", signum);
 	exit(EXIT_SUCCESS);
+}
+
+int sigisemptyset(sigset_t *setp)
+{
+	return setp->__val[0] == 0;
+}
+
+void dec_to_binary(unsigned long int num)
+{
+	uint8_t binary_num[64];
+	int count_bit = 0;
+
+	while (num > 0)
+	{
+		binary_num[count_bit] = num % 2;
+		num = num / 2;
+		count_bit++;
+	}
+
+	for (int j = count_bit - 1; j >= 0; j--)
+	{
+		printf("%d ", binary_num[j]);
+	}
+	printf("\n");
+}
+
+void print_set_word(sigset_t *setp)
+{
+	int i;
+
+	if (!setp)
+	{
+		fprintf(stderr, "print_set_word(): NULL parameter\n");
+		return;
+	}
+
+	/* Print the current value of signal mask of the process */
+	for (i = 0; i < 1; i++)
+	{
+		printf("%lu", setp->__val[i]);
+	}
+
+	printf("\n[%s]\n\n", sigisemptyset(setp) ? "Empty Set" : "Non-empty Set");
 }
