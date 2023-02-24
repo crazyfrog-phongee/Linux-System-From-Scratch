4e19fd200b7b357c4a57f165bfba86193c95eb89
crazyfrog-phongee <trongphong1708@gmail.com>
2023-02-24 13:50:13 +0700

    Finish assignment 2: Check signal pending of multithread

---
+++ week-2/2-thread/Readme.md
@@ -0,0 +1,62 @@
+# DESCRIPTION: 
+## Viết một chương trình để chứng minh rằng các thread khác nhau trong cùng một process có thể có các tập hợp signal đang chờ xử lý (set of pending signal) khác nhau, được trả về bằng sigpending(). Bạn có thể làm điều này bằng cách sử dụng pthread_kill() để gửi các tín hiệu khác nhau đến hai thread khác nhau đã bị block các tín hiệu này, và sau với mỗi thread gọi sigpending() hãy hiển thị thông tin về các tín hiệu đang chờ xử lý.
+
+## SOLUTION IDEA:
+
+* Main thread: set signal mask để chặn tín hiệu SIGTERM bằng cách sử dụng hàm pthread_sigmask()
+
+* Từ main thread tạo ra 2 thread mới với id tương ứng thread_id1 và thread_id2 bằng cách sử dụng hàm pthread_create()
+**Khi đó, 2 thread mới được tạo sẽ kế thừa signal mask và disposition của main thread**
+
+* Ở các hàm start_routine của 2 thread mới, set signal mask khác nhau:
+Cụ thể, với thread_id1: thêm block SIGUSR1 bằng cách sử dụng hàm sigaddset()
+Cụ thể, với thread_id2: thêm block SIGUSR2 bằng cách sử dụng hàm sigaddset()
+
+Trong vòng lặp while(true) của các thread, kiểm tra các signal pending bằng cách sử dụng sigpending();
+
+* Quay lai main thread, sau khi tạo thành công 2 thread, sử dụng hàm pthread_kill() để send signal tới 2 thread vừa tạo ra.
+
+# THEORY:
+
+## Process's signal disposition
+
+### process's signal disposition: similar to the signal's handler
+* The signal disposition can be changed by calling signal() or sigaction
+
+* REF: 
+
+`https://csresources.github.io/SystemProgrammingWiki/SystemProgramming/Signals,-Part-2:-Pending-Signals-and-Signal-Masks/`
+
+### Pending state
+
+* The most common reason for a signal to be pending is that the process (or thread) has currently blocked that particular signal.
+
+* **Signals can be blocked (meaning they will stay in the pending state) by setting the process signal mask or, when you are writing a multi-threaded program, the thread signal mask.**
+
+## Children's signal disposition
+
+### What happens during fork ?
+
+* The child process inherits a copy of the parent process's signal disposition and a copy of the parent's signal mask.
+
+* Pending signals however are not inherited by the child.
+
+## Thread's signal disposition
+
+### What happens when creating a new thread?
+
+* The new thread inherits a copy of the calling thread's mask
+
+### Block signals in a multi-threaded program
+* **Blocking signals is similar in multi-threaded programs to single-threaded programs: Use pthread_sigmask instead of sigprocmask Block a signal in all threads to prevent its asynchronous delivery.**
+
+### How are pending signals delivered in a multi-threaded program?
+
+* A signal is delivered to any signal thread that is not blocking that signal.
+
+* If the two or more threads can receive the signal then which thread will be interrupted is arbitrary!
+
+## sigpending():
+
+* returns a set up of signals that are pending for delivery to the call thread (i.e., the signals which have been raised while blocked).  The mask of pending signals is returned in set.
+* RETURN VALUE: 0 on sucess, -1 on failure
---
+++ week-2/2-thread/main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <signal.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
+
+#define handle_error_en(en, msg) \
+        do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+pthread_t thread_id1, thread_id2;
+
+void print_signal_mask(unsigned long int num)
+{
+	uint8_t binary_num[64];
+	int count_bit = 0;
+
+	while (num > 0)
+	{
+		binary_num[count_bit] = num % 2;
+		num = num / 2;
+		count_bit++;
+	}
+
+	for (int j = count_bit - 1; j >= 0; j--)
+	{
+		printf("%d ", binary_num[j]);
+	}
+	printf("\n");
+}
+
+void check_pending(pthread_t tid, int sig, char *signame ) {
+    sigset_t sigset;
+
+    if (sigpending(&sigset) != 0)
+        perror("sigpending() error\n" );
+    else if (sigismember(&sigset, sig ))
+        printf( "%ld: a %s signal is pending\n", tid, signame);
+    else
+        printf( "%ld: no %s signals are pending\n", tid, signame);
+}
+
+void *thread_handle1(void *arg)
+{
+    printf("Hello from ThreadID1 with %ld\n", pthread_self());
+
+    sigset_t *set = (sigset_t *)(arg);
+    sigset_t sig_pending;
+
+    sigaddset(set, SIGUSR1);
+    sigprocmask(SIG_BLOCK, set, NULL);
+
+    // print_signal_mask(set->__val[0]);
+
+    for(;;)
+    {
+
+        if (sigpending(&sig_pending) != 0)
+            perror("sigpending() error\n" );
+        
+        if (sig_pending.__val[0] == 0)
+            printf("ThreadID1: No signals are pending\n");
+        else 
+        {
+            printf("ThreadID1: Signals are pending\n");
+            print_signal_mask(sig_pending.__val[0]);
+        }
+
+        sleep(5);
+    }
+}
+
+void *thread_handle2(void *arg)
+{
+    printf("Hello from ThreadID2 with %ld\n", pthread_self());
+
+    sigset_t *set = (sigset_t *)(arg);
+    sigset_t sig_pending;
+
+    sigaddset(set, SIGUSR2);
+    sigprocmask(SIG_BLOCK, set, NULL);
+
+    // print_signal_mask(set->__val[0]);
+
+    for(;;)
+    {
+        if (sigpending(&sig_pending) != 0)
+            perror("sigpending() error\n" );
+
+        if (sig_pending.__val[0] == 0)
+            printf("ThreadID2: No signals are pending\n");
+        else 
+        {
+            printf("ThreadID2: Signals are pending\n");
+            print_signal_mask(sig_pending.__val[0]);
+        }
+
+        sleep(2);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int ret;
+    sigset_t oldset, newset;
+
+    sigemptyset(&newset);
+    sigemptyset(&oldset);
+
+    sigaddset(&newset, SIGTERM);
+
+    if ((ret = pthread_sigmask(SIG_BLOCK, &newset, NULL)))
+    {
+        handle_error_en(ret, "pthread_sigmask");
+    }
+
+    if ((ret = pthread_create(&thread_id1, NULL, thread_handle1, &newset)))
+    {
+        handle_error_en(ret, "pthread_create");
+    }
+
+    if ((ret = pthread_create(&thread_id2, NULL, thread_handle2, &newset)))
+    {
+        handle_error_en(ret, "pthread_create");
+    }
+
+    if (sigprocmask(SIG_SETMASK, NULL, &oldset) == 0)
+    {
+        printf("Signal mask in main thread: ");
+        print_signal_mask(oldset.__val[0]);
+    }
+
+    sleep(2);
+
+    pthread_kill(thread_id1, SIGTERM);
+
+    sleep(5);
+
+    pthread_kill(thread_id2, SIGUSR2);
+    pthread_kill(thread_id1, SIGUSR1);
+
+    while (1);
+
+    return 0;
+}
